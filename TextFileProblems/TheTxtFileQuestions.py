333a04bde55b057aa9adb69ac1c38e0317232083
Suhrid Sadman Abrar <suhridabrarbd@outlook.com>
2022-11-27 14:15:45 +0530

    Txt files update

---
+++ TextFileProblems/TheTxtFileQuestions.py
@@ -0,0 +1,12 @@
+# Question 1 [Paper 2 May/June 2022 9618/22]
+'''A procedure LastLines() will:
+• take the name of a text file as a parameter
+• output the last three lines from that file, in the same order as they appear in the file.
+ Note:
+• Use local variables LineX, LineY and LineZ to store the three lines from the file.
+• You may assume the file exists and contains at least three lines.
+ Write Python code for the procedure LastLines().'''
+
+f = open("TheTxtFile.txt", "r")
+
+print(len(f))
